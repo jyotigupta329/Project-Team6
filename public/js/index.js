e78a0525647f16c43bbe71d6ab352601751bbbec
jyoti <jyoti.gupta329@gmail.com>
2018-09-11 11:27:56 -0500

    Added sptify API onclick of inspire button

--- public/js/index.js
+++ public/js/index.js
@@ -1,65 +1,22 @@
-// Get references to page elements
-var $exampleText = $("#example-text");
-var $exampleDescription = $("#example-description");
-var $submitBtn = $("#submit");
-var $exampleList = $("#example-list");
 
-//read and set any environment variables with the dotenv package:
-require("dotenv").config();
-
-// --------------------------------------------------------//
-// Dependencies for Spotify API
-//--------------------------------------------------------//
-var Spotify = require('node-spotify-api');
-var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
-
-
-
-// Add event listeners to the submit and delete buttons
-
-$("#submit").on("click", function (event) {
-  event.preventDefault();
-  var temp = $("#feeling").val().trim();
-  var userFeeling = {
-    feeling: temp
-  };
-  $.ajax("/api/quotes", {
-    type: "POST",
-    data: userFeeling
-  }).then(function (res) {
-    console.log("User Feeling detected.");
-    console.log(res.data);
-    spotifySong(res.data);
-  });
+$(document).ready(function () {
+  $('#login').webuiPopover({ url: '#login-form' });
 });
 
 
-// --------------------------------------------------------//
-// Function to call spotify API
-//--------------------------------------------------------//
-
-function spotifySong(term) {
-
-  if (!term) {
-    term = "The Sign Ace Of Base";
-  }
-
-  spotify
-    .search({ type: 'track', query: term })
-    .then(function (response) {
-
-      var jsonData = response.tracks.items[0];
-      console.log(jsonData);
-      var logSpotify =
-        chalk.bold.yellow("\nArtist(s): ") + jsonData.album.artists[0].name +
-        chalk.bold.yellow("\nThe song's name: ") + jsonData.name +
-        chalk.bold.yellow("\nThe album that the song is from: ") + jsonData.album.name;
-      console.log("\n----------------------- movie-this command -----------------------------");
-      console.log(logSpotify);
-      console.log("\n-----------------------------------------------------------------------\n");
-    })
-    .catch(function (err) {
-      console.log(err);
+$(function () {
+  $("#submit").on("click", function (event) {
+    event.preventDefault();
+    console.log("inspired");
+    var temp = $("#feeling").val().trim();
+    // var userFeeling = {
+    //   feeling: temp
+    // };
+    $.ajax("/api/findSong/" + temp, {
+      type: "GET"
+    }).then(function (res) {
+      console.log("User Feeling detected.");
+      console.log(res);
     });
-}
+  });
+});
