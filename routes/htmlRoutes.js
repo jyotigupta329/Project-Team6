9c516b9cef6ad52783a3b0c53dd492e383bace3a
must-git-good <39744893+must-git-good@users.noreply.github.com>
2018-09-13 10:40:35 -0500

    Hit some more merge bugs. Testing again before pushing.

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,7 +2,7 @@ var db = require("../models");
 
 //Auth Dependencies. We may move passport to its own file, but getting it working for now:
 //////////////////////////////////////////////////////////////////////////////////////////
-var check = require("../authcheck.js");
+// var check = require("../authcheck.js");
 const passport = require("passport");
 const { Strategy: JwtStrategy, ExtractJwt } = require("passport-jwt")
 
@@ -25,34 +25,7 @@ passport.use(new JwtStrategy(passportOps, (jwt_payload, done) => {
   })
 }));
 
-//and auth-based routing, as well:
 
-// app.post("/verified", passport.authenticate('jwt', {session: false}), function(req, res) {
-//   // req.setRequestHeader('Authorization', 'Basic eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZXhwaXJlcyI6MTUzOTM3MTEwODIwMCwiaWF0IjoxNTM2Nzc5MTA4fQ.fQ63RLS7Jd8QeZ0LlOAIKa_PL9YzEgQiWRAzq38Kc9c' );
-//   console.log(req.body.jwtToken);
-//   // const { userName, password } = req.userName;
-//   res.render("verified");
-// });
-
-// app.post("/verified", function (req, res){
-//     ;
-// });
-
-// // The route to go to a page for logged in users:  STILL IN DEVELOPMENT
-// app.post("/verified", passport.authenticate('jwt', { session: false }), function(req, res) {
-//   //WRAP THIS IN IF LOGIC TO GO BASED ON JWT AUTH.
-//   res.render("verified");
-// });
-
-
-// Load example page and pass in an example by id
-// app.get("/example/:id", function(req, res) {
-//   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-//     res.render("example", {
-//       example: dbExample
-//     });
-//   });
-// });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////
 
